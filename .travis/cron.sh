f35253740803a7b3cbb45e1bd2fd19425d308fe8
Kyle Fazzari <kyle@canonical.com>
2016-11-18 13:17:41 -0800

    Update version to 10.0.1snap4.
    Fix Travis cron daily deployments.
    
    Signed-off-by: Kyle Fazzari <kyle@canonical.com>

--- .travis/cron.sh
+++ .travis/cron.sh
@@ -3,23 +3,30 @@
 latest_master_url="https://download.nextcloud.com/server/daily/latest-master.tar.bz2"
 latest_stable_url="https://download.nextcloud.com/server/daily/latest-stable10.tar.bz2"
 
+rewrite_snapcraft_yaml()
+{
+	sed -ri "s|(source:\s+).*download.nextcloud.com.*$|\1$1|" snapcraft.yaml
+}
+
 echo "Requesting build of latest master..."
+git checkout -b edge origin/${TRAVIS_BRANCH}
 
 # Rewrite the snapcraft.yaml to pull from the latest master.
-sed -ri "s|(source:\s+).*download.nextcloud.com.*$|\1$latest_master_url|" snapcraft.yaml
+rewrite_snapcraft_yaml $latest_master_url
 
 # Commit the changes and push to edge to begin the edge build.
 git add .
 git commit -m 'From CI: Use Nextcloud latest master'
-git push deploy $TRAVIS_BRANCH:edge --force
+git push deploy edge --force
 
 
 echo "Requesting build of latest stable..."
+git checkout -b beta origin/${TRAVIS_BRANCH}
 
 # Now rewrite the snapcraft.yaml to pull from the latest stable v10.
-sed -ri "s|(source:\s+).*download.nextcloud.com.*$|\1$latest_stable_url|" snapcraft.yaml
+rewrite_snapcraft_yaml $latest_stable_url
 
 # Commit the changes and push to beta to begin the beta build.
 git add .
 git commit -m 'From CI: Use Nextcloud latest stable'
-git push deploy $TRAVIS_BRANCH:beta --force
+git push deploy beta --force

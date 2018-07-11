08464d6320e4f30f85e68e8b4092cd1aed65243c
bn4t <info@apply55gx.com>
2018-07-11 19:59:08 +0200

    update http/2 spec

--- tests/spec/enable_http2_spec.rb
+++ tests/spec/enable_http2_spec.rb
@@ -6,8 +6,8 @@ feature "HTTP2" do
 		enable_http2
 
 		# test if http/2 is supported using a curl docker image (with http/2 support compiled in)
-		`docker run -t --rm registry.gitlab.com/bn4t/curl-http2-docker -sI https://localhost -o/dev/null -w '%{http_version}'`
-		expect($?).to eq "2"
+		http_version = `docker run -t --rm registry.gitlab.com/bn4t/curl-http2-docker -sI https://localhost -o/dev/null -w '%{http_version}'`
+		expect(http_version).to eq "2"
 	end
 
 	scenario "Disable HTTP2" do
@@ -16,7 +16,7 @@ feature "HTTP2" do
 		disable_http2
 
 		# test if http/2 is supported using a curl docker image (with http/2 support compiled in)
-		`docker run -t --rm registry.gitlab.com/bn4t/curl-http2-docker -sI https://localhost -o/dev/null -w '%{http_version}'`
-		expect($?).to eq "1.1"
+		http_version = `docker run -t --rm registry.gitlab.com/bn4t/curl-http2-docker -sI https://localhost -o/dev/null -w '%{http_version}'`
+		expect(http_version).to eq "1.1"
 	end
 end

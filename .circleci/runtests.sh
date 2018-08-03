949e358f9a660000beaed0be0816df02c07d2919
Kyle Fazzari <kyrofa@ubuntu.com>
2018-08-03 12:59:11 -0700

    Use bash
    Signed-off-by: Kyle Fazzari <kyrofa@ubuntu.com>

--- .circleci/runtests.sh
+++ .circleci/runtests.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 # Need to work around https://github.com/thoughtbot/capybara-webkit/issues/494
 # which requires a newer qt5 than that available in Trusty. Webkit was removed
 # in 5.6 though, so stick with 5.5.
-sudo add-apt-repository ppa:beineri/opt-qt551-trusty
+sudo add-apt-repository --yes ppa:beineri/opt-qt551-trusty
 sudo apt update
 
 # Also install xvfb since we're running headless in CI
-sudo apt install qt55-meta-minimal qt55webkit xvfb -y
+sudo apt install --yes qt55-meta-minimal qt55webkit xvfb
 
 # Finally, activate the Qt installation
-. /opt/qt55/bin/qt55-env.sh
+source /opt/qt55/bin/qt55-env.sh
 
 # In order to use rvm, we need a login shell. We need to install
 # Ruby v2.4.0 (the older version that is the default doesn't handle

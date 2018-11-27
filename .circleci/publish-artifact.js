52fd0a5cc2d6491f6fbd4f8f361fc4e155178402
bn4t <17193640+bn4t@users.noreply.github.com>
2018-11-27 14:04:43 +0000

    Delete publish-artifact.js

--- .circleci/publish-artifact.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-
-const bot = require("circle-github-bot").create();
-
-bot.comment(`
-<h3>${bot.env.commitMessage}</h3>
-Artifact: <strong>${bot.artifactLink('/artifacts/nextcloud.snap', 'Download')}</strong>
-`);

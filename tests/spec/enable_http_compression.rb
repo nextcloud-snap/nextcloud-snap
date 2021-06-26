f8153b8ad832033703a3f31b6bf52b24311382fb
Marc Peña Segarra <segarrra@gmail.com>
2021-06-26 23:39:00 +0200

    Add test for HTTP compression

---
+++ tests/spec/enable_http_compression.rb
@@ -0,0 +1,27 @@
+require 'net/http'
+
+feature "Change http compression" do
+	after(:all) do
+                set_config "apache.compression": "false"
+                wait_for_nextcloud
+	end
+
+	scenario "compression" do
+                set_config "apache.compression": "true"
+                wait_for_nextcloud
+
+		assert_apache_compression
+	end
+
+	protected
+
+	def assert_apache_compression
+		# Verify that Apache is returning compressed reponses
+		expect(nextcloud_response["content-encoding"]).to eq "br"
+	end
+
+
+	def nextcloud_response(url: "http://localhost")
+		return Net::HTTP.get_response(URI(url))
+	end
+end

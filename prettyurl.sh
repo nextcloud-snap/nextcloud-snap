6877afbfaa0e05d82dd52307efdf1d20144506c2
szaimen <szaimen@e.mail.de>
2021-07-18 21:13:26 +0200

    Install PHP stack and get config from htaccess file
    Signed-off-by: szaimen <szaimen@e.mail.de>

--- prettyurl.sh
+++ prettyurl.sh
@@ -5,6 +5,9 @@
 # git clone --branch prettyurl https://github.com/nextcloud/nextcloud-snap.git
 # cd nextcloud-snap
 
+# Stop if something fails
+set -e
+
 # Get the correct tar file from the branch itself
 NC_DOMAIN="$(grep 'https://download.nextcloud.com/server' ./snap/snapcraft.yaml | grep -oP 'https://.*')"
 
@@ -14,35 +17,47 @@ curl -fL "$NC_DOMAIN" -o /tmp/ncpackage.tar.bz2
 # Extract the archive
 mkdir -p /tmp/nc
 tar -xjf /tmp/ncpackage.tar.bz2 -C /tmp/nc
+sudo chown -R www-data:www-data /tmp/nc
+sudo chmod -R 770 /tmp/nc
 
-# Test if the needed file exists
-if ! [ -f /tmp/nc/nextcloud/lib/private/Setup.php ]
-then
-    echo "The Setup couldn't get extracted."
-    exit 1
-fi
+# Set up PHP-FPM
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends \
+    php-fpm \
+    php-intl \
+    php-ldap \
+    php-imap \
+    php-gd \
+    php-sqlite3 \
+    php-curl \
+    php-xml \
+    php-zip \
+    php-mbstring \
+    php-soap \
+    php-json \
+    php-gmp \
+    php-bz2 \
+    php-bcmath \
+    php-pear;
 
-# Alway create a new htaccess file
-rm -f ./src/apache/conf/.htaccess
+# Install Nextcloud
+sudo -u www-data \
+    php -f /tmp/nc/nextcloud/occ \
+    maintenance:install \
+    --database=sqlite \
+    --admin-user=admin \
+    --admin-pass=password
 
-# Get content of the updateHtaccess function and store it in a Variable called updateHtaccess
-updateHtaccess="$(sed -n "/function updateHtaccess/,/function/p" /tmp/nc/nextcloud/lib/private/Setup.php)"
+# Enable pretty URLS
+sudo -u www-data php -f /tmp/nc/nextcloud/occ config:system:set htaccess.RewriteBase --value="/"
+sudo -u www-data php -f /tmp/nc/nextcloud/occ maintenance:update:htaccess
 
-# Get all needed lines (containing the content variable)
-updateHtaccess="$(echo "$updateHtaccess" | grep '$content =\|$content \.=')"
+# Get content of the changed htaccess file and store it in a Variable called updateHtaccess
+updateHtaccess="$(sudo sed -e '1,/DO NOT CHANGE ANYTHING ABOVE THIS LINE/d' /tmp/nc/nextcloud/.htaccess)"
 
 # Create file with final prettyurl config
 echo "# Retreived from $NC_DOMAIN" >> /tmp/htaccess.conf
 echo "$updateHtaccess" >> /tmp/htaccess.conf
-
-# Remove comment line
-sed -i '/DO NOT CHANGE ANYTHING ABOVE THIS LINE/d' /tmp/htaccess.conf
-
-# Edit file to be valid
-sed -i 's|.*"\\n||' /tmp/htaccess.conf
-sed -i 's|;$||' /tmp/htaccess.conf
-sed -i 's|"$||' /tmp/htaccess.conf
-sed -i 's|\\\\|\\|g' /tmp/htaccess.conf
 
 # Overwrite Webroot config
 sed -i 's|ErrorDocument 403.*|ErrorDocument 403 \${WEBROOT}/|' /tmp/htaccess.conf
@@ -51,5 +66,5 @@ sed -i 's|ErrorDocument 404.*|ErrorDocument 404 \${WEBROOT}/|' /tmp/htaccess.con
 # Overwrite Rewritebase config
 sed -i 's|RewriteBase.*|RewriteBase \${REWRITEBASE}|' /tmp/htaccess.conf
 
-# Add the new config to the file
-cat /tmp/htaccess.conf >> ./src/apache/conf/.htaccess
+# Overwrite changes
+cat /tmp/htaccess.conf > ./src/apache/conf/.htaccess

db21f5005748de608646f84d6479f8307a5bdb94
Marc Peña Segarra <segarrra@gmail.com>
2021-09-14 15:54:08 +0200

    Fix indentation

--- tests/integration/enable_http_compression.rb
+++ tests/integration/enable_http_compression.rb
@@ -2,13 +2,13 @@ require 'net/http'
 
 feature "Change http compression" do
 	after(:all) do
-                set_config "http.compression": "false"
-                wait_for_nextcloud
+		set_config "http.compression": "false"
+		wait_for_nextcloud
 	end
 
 	scenario "compression" do
-                set_config "http.compression": "true"
-                wait_for_nextcloud
+		set_config "http.compression": "true"
+		wait_for_nextcloud
 
 		assert_apache_compression
 	end
@@ -19,7 +19,6 @@ feature "Change http compression" do
 		# Verify that Apache is returning compressed reponses
 		expect(nextcloud_response["content-encoding"]).to eq "br"
 	end
-
 
 	def nextcloud_response(url: "http://localhost")
 		return Net::HTTP.get_response(URI(url))

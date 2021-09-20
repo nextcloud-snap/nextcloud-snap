a491d6949a10149e2281f00fa8259e1450e4d368
Marc Peña Segarra <segarrra@gmail.com>
2021-09-20 22:55:55 +0200

    add invalid test for http compression

--- tests/integration/enable_http_compression.rb
+++ tests/integration/enable_http_compression.rb
@@ -6,6 +6,13 @@ feature "Change http compression" do
 		wait_for_nextcloud
 	end
 
+	scenario "invalid" do
+		# This will print to stderr. Redirect so we can capture it easily.
+		output=`sudo snap set nextcloud http.compression=invalid 2>&1`
+		expect($?.to_i).to_not eq 0
+		expect(output).to include "value must be either 'true' or 'false'"
+	end
+
 	scenario "compression" do
 		set_config "http.compression": "true"
 		wait_for_nextcloud
@@ -17,10 +24,11 @@ feature "Change http compression" do
 
 	def assert_apache_compression
 		# Verify that Apache is returning compressed reponses
-		expect(nextcloud_response["content-encoding"]).to eq "br"
+		response = nextcloud_response
+		expect(response["content-encoding"]).to include "br"
 	end
 
-	def nextcloud_response(url: "http://localhost")
+	def nextcloud_response(url: "http://localhost/core/img/favicon.ico")
 		return Net::HTTP.get_response(URI(url))
 	end
 end

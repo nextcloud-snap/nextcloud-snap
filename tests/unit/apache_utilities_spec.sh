47ef8590171135dbebf93895c6c02e64de4f48be
Kyle Fazzari <kyrofa@ubuntu.com>
2021-10-09 12:11:27 -0700

    tests: finish unit tests for apache utilities
    Signed-off-by: Kyle Fazzari <kyrofa@ubuntu.com>

--- tests/unit/apache_utilities_spec.sh
+++ tests/unit/apache_utilities_spec.sh
@@ -7,6 +7,77 @@ Describe 'apache-utilities'
 		snapctl reset
 	}
 
+	Describe 'apache pid function'
+		BeforeEach 'setup'
+		AfterEach 'teardown'
+		setup()
+		{
+			APACHE_PIDFILE="$(mktemp)"
+		}
+		teardown()
+		{
+			rm -f "$APACHE_PIDFILE"
+		}
+
+		Describe 'apache_pid'
+			It 'fails gracefully when apache is not running'
+				rm -f "$APACHE_PIDFILE"
+
+				When call apache_pid
+				The output should equal ''
+				The error should equal "Unable to get Apache PID as it's not yet running"
+			End
+
+			It 'returns pid when apache is running'
+				echo '42' > "$APACHE_PIDFILE"
+				When call apache_pid
+				The output should equal '42'
+			End
+		End
+
+		Describe 'apache_is_running'
+			It 'fails when apache is not running'
+				rm -f "$APACHE_PIDFILE"
+
+				When call apache_is_running
+				The status should be failure
+				The output should equal ''
+			End
+
+			It 'succeeds when apache is running'
+				When call apache_is_running
+				The status should be success
+				The output should equal ''
+			End
+		End
+	End
+
+	Describe 'apache_http_port'
+		It 'defaults to 80'
+			When call apache_http_port
+			The output should equal '80'
+		End
+
+		It 'supports being changed'
+			apache_set_http_port 81
+			When call apache_http_port
+			The output should equal '81'
+		End
+	End
+
+	Describe 'apache_https_port'
+		It 'defaults to 443'
+			When call apache_https_port
+			The output should equal '443'
+		End
+
+		It 'supports being changed'
+			apache_set_https_port 444
+			When call apache_https_port
+			The output should equal '444'
+		End
+	End
+
 	Describe 'apache_get_http_compression'
 		It 'defaults to false'
 			When call apache_get_http_compression

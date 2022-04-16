nextcloud-snap/nextcloud-snap

commit ff1d2a62736136b06ae46da732c8d0a08c88d877

apache: add proper pcre deps
libpcre2 is actually NEWER than libpcre3. Who knew.

Signed-off-by: Kyle Fazzari <kyrofa@ubuntu.com>

snap/snapcraft.yaml
@@ -139,9 +139,11 @@ parts:
 
     build-packages:
       - libbrotli-dev
+      - libpcre2-dev
 
     stage-packages:
       - libbrotli1
+      - libpcre2-8-0
 
       # The built-in Apache modules to enable
     modules:

commit e1f12a5db5eb4564891fc3298a9a3f2901d654da

snapcraft: move apache build/stage packages into plugin
This was a mistake introduced in
ff1d2a62736136b06ae46da732c8d0a08c88d877. Fix it.

Signed-off-by: Kyle Fazzari <kyrofa@ubuntu.com>

snap/plugins/apache.py
@@ -36,10 +36,9 @@ class ApachePlugin(snapcraft.plugins.v1.PluginV1):
         super().__init__(name, options, project)
 
         self.build_packages.extend(
-            ['pkg-config', 'libapr1-dev', 'libaprutil1-dev', 'libpcre3-dev',
+            ['pkg-config', 'libapr1-dev', 'libaprutil1-dev', 'libpcre2-dev',
              'libssl-dev'])
-        self.stage_packages.extend(['libapr1', 'libaprutil1'])
-
+        self.stage_packages.extend(['libapr1', 'libaprutil1', 'libpcre2-8-0'])
 
     def build(self):
         super().build()

snap/snapcraft.yaml
@@ -139,11 +139,9 @@ parts:
 
     build-packages:
       - libbrotli-dev
-      - libpcre2-dev
 
     stage-packages:
       - libbrotli1
-      - libpcre2-8-0
 
       # The built-in Apache modules to enable
     modules:
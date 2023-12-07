9cfb5e74b899d4d8490b89815b801103ab3e4a70
Pascal <pilzinsel64@gmx.de>
2023-12-07 18:41:24 +0100

    ci: disable dailies for nextcloud 25

--- .github/spawn-dailies.sh
+++ .github/spawn-dailies.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 latest_master_url="https://download.nextcloud.com/server/daily/latest-master.tar.bz2"
-latest_stable25_url="https://download.nextcloud.com/server/daily/latest-stable25.tar.bz2"
 latest_stable26_url="https://download.nextcloud.com/server/daily/latest-stable26.tar.bz2"
 latest_stable27_url="https://download.nextcloud.com/server/daily/latest-stable27.tar.bz2"
 
@@ -36,11 +35,6 @@ echo "Requesting build of latest master..."
 request_build \
 	"latest-master" "$latest_master_url" "master-$today" \
 	"From CI: Use Nextcloud latest master"
-
-echo "Requesting build of latest 25..."
-request_build \
-	"latest-25" "$latest_stable25_url" "25-$today" \
-	"From CI: Use Nextcloud latest 25"
 
 echo "Requesting build of latest 26..."
 request_build \

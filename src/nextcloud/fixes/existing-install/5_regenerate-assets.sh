c4a08dc72bd575a42a9133f1504dbd34f8d72c2d
Pascal <pilzinsel64@gmx.de>
2024-09-22 16:04:28 +0100

    nextcloud: include expensive steps on repair

--- src/nextcloud/fixes/existing-install/5_regenerate-assets.sh
+++ src/nextcloud/fixes/existing-install/5_regenerate-assets.sh
@@ -4,4 +4,4 @@
 # regenerates the asset caches. This is required because these caches are
 # stored alongside the data, which is unversioned. Without clearing the caches
 # on starup, a revert would try to use a newer version's CSS, for example.
-occ -n maintenance:repair
+occ -n maintenance:repair --include-expensive

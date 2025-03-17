c11d812170bdf984938984bcab2171937dc43784
stondino00 <54154986+stondino00@users.noreply.github.com>
2025-03-16 20:09:50 -0500

    Update import_export_spec.rb

--- tests/integration/import_export_spec.rb
+++ tests/integration/import_export_spec.rb
@@ -40,6 +40,6 @@ feature "Import and export data" do
 		fill_in "user", with: "admin"
 		fill_in "password", with: "admin"
 		click_button "Log in"
-		expect(page).to have_content /(Recommended|All) files/
+		expect(page).to have_content /(Recommended|All) files/, wait: 10
 	end
 end
